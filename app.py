58a2dd0080c807e743193ebd0b549d96a293bc0d
luiz930 <81279096+luiz930@users.noreply.github.com>
2026-03-24 22:19:56 -0300

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -231,4 +231,7 @@ def cadastrar_pneu():
 
     return render_template("pneu.html", produtos=lista)
 
-app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
